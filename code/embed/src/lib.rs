9cca28500107a0793b28b4438e2a5c23bb1ec157
Clay Dunston <dunstontc@gmail.com>
2018-05-05 01:24:56 -0500

    tidy up

--- code/embed/src/lib.rs
+++ code/embed/src/lib.rs
@@ -2,7 +2,8 @@ use std::thread;
 
 #[no_mangle]
 pub extern fn process() {
-    let handles: Vec<_> = (0..10).map(|_| {
+
+    let handles : Vec<_> = (0..10).map(|_| {
         thread::spawn(|| {
             let mut x = 0;
             for _ in 0..5_000_000 {
@@ -16,5 +17,6 @@ pub extern fn process() {
         println!("Thread finished with count={}",
         h.join().map_err(|_| "Could not join a thread!").unwrap());
     }
+
     println!("done!");
 }
