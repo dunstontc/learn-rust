32ab859636ed4777f7e8a7ee32bea4d14fa751f4
Clay Dunston <dunstontc@gmail.com>
2018-05-05 01:27:22 -0500

    cleaner

--- code/guessing_game/src/main.rs
+++ code/guessing_game/src/main.rs
@@ -1,11 +1,8 @@
 extern crate rand;
 
 use std::io;
-// Ordering is another enum, like Result, but the variants for Ordering are Less, Greater, and Equal.
-// These are the three outcomes that are possible when you compare two values.
-use std::cmp::Ordering;
-// Rng is a *trait* that defines methods that random number generators implement, and this trait must be in scope for us to use those methods.
-use rand::Rng;
+use std::cmp::Ordering;  // Ordering is another enum, like Result, but the variants for Ordering are Less, Greater, and Equal.
+use rand::Rng;  // Rng is a *trait* that defines methods that random number generators implement, and this trait must be in scope for us to use those methods.
 
 fn main() {
     println!("Guess the number!");
@@ -25,7 +22,8 @@ fn main() {
         // which gives you a way to let multiple parts of your code access one
         // piece of data without needing to copy that data into memory multiple times.
         // Like variables, references are immutable by default.
-        io::stdin().read_line(&mut guess)
+        io::stdin()
+            .read_line(&mut guess)
             .expect("Failed to read line");
 
             // read_line returns an `io::Result` value. (an enum)
@@ -36,9 +34,9 @@ fn main() {
         // The parse method on strings parses a string into some kind of number.
         // Because this method can parse a variety of number types,
         // we need to tell Rust the exact number type we want by using let guess: u32.
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,      // `Ok` indicates the operation was successful, and inside the `Ok` variant is the successfully generated value.
-            Err(_) => continue,  // `Err` means the operation failed, and `Err` contains information about how or why the operation failed.
+        let guess : u32 = match guess.trim().parse() {
+            Ok(num) => num,       // `Ok` indicates the operation was successful, and inside the `Ok` variant is the successfully generated value.
+            Err(_)  => continue,  // `Err` means the operation failed, and `Err` contains information about how or why the operation failed.
         };
 
         println!("You guessed: {}", guess);
