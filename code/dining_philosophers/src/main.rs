ed823ade650b37a289c4574cbb2d045b454a843f
Clay Dunston <dunstontc@gmail.com>
2018-05-04 23:04:32 -0500

    dinner time

---
+++ code/dining_philosophers/src/main.rs
@@ -0,0 +1,68 @@
+use std::{thread, time};
+use std::sync::{Mutex, Arc};
+
+struct Philosopher {
+    name  : String, // Generally speaking, working with a type which owns its data is easier than working with one that uses references.
+    left  : usize,
+    right : usize,
+}
+
+impl Philosopher {
+    // This name, `new()`, isn’t anything special to Rust, but it is a convention for functions that create new instances of structs.
+    fn new(name : &str, left : usize, right : usize) -> Philosopher {
+        Philosopher {
+            name  : name.to_string(),
+            left  : left,
+            right : right,
+        }
+    }
+
+    fn eat(&self, table : &Table) {
+        let two_seconds = time::Duration::from_millis(2000);
+
+        let _left = table.forks[self.left].lock().unwrap();
+        let _right = table.forks[self.right].lock().unwrap();
+
+        println!("{} is eating.", self.name);
+        thread::sleep(two_seconds);
+        println!("{} is done eating.", self.name);
+    }
+}
+
+struct Table {
+    forks : Vec<Mutex<()>>,
+}
+
+fn main() {
+    let table = Arc::new(Table { forks : vec![
+        Mutex::new(()),
+        Mutex::new(()),
+        Mutex::new(()),
+        Mutex::new(()),
+        Mutex::new(()),
+    ]});
+
+    let philosophers = vec![
+        Philosopher::new("Bill S. Preston Esq.", 0, 1),
+        Philosopher::new("Ted \"Theodore\" Logan", 1, 2),
+        Philosopher::new("Socrates", 2, 3),
+        Philosopher::new("Joan of Arc", 3, 4),
+        Philosopher::new("Abraham Lincoln", 0, 4), // This is what prevents deadlock: one of our philosophers is left handed!
+    ];
+
+    // The _ is a type placeholder.
+    let handles : Vec<_> = philosophers
+        .into_iter()
+        .map(|p| {
+            let table = table.clone();
+
+            thread::spawn(move || {
+                p.eat(&table);
+            })
+        })
+        .collect();
+
+    for h in handles {
+        h.join().unwrap();
+    }
+}
