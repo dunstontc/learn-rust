c8ffd6565b7efb3053e99f7053e4684c0eac99c7
Clay Dunston <dunstontc@gmail.com>
2018-05-20 16:50:34 -0500

    code from the Book volumes 1 & 2 to new folders

---
+++ code/TheBookV2/CommonCollections/haitian/readme.md
@@ -0,0 +1,13 @@
+# [Storing Keys with Associated Values in Hash Maps](https://doc.rust-lang.org/book/second-edition/ch08-03-hash-maps.html)
+
+
+## Creating a New Hash Map
+`HashMap<K, V>`
+## Hash Maps and Ownership
+## Accessing Values in a Hash Map
+## Updating a Hash Map
+  - Overwriting a Value
+  - Only Inserting a Value If the Key Has No Value
+  - Updating a Value Based on the Old Value
+## Hashing Functions
+
---
+++ code/TheBookV2/CommonCollections/haitian/src/main.rs
@@ -0,0 +1,21 @@
+use std::collections::HashMap;
+
+fn main() {
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+
+    let teams  = vec![String::from("Blue"), String::from("Yellow")];
+    let initial_scores = vec![10, 50];
+
+    let scores: HashMap<_, _> = teams.iter().zip(initial_scores.iter()).collect();
+
+    let field_name = String::from("Favorite color");
+    let field_value = String::from("Blue");
+
+    let mut map = HashMap::new();
+    map.insert(field_name, field_value);
+    // field_name and field_value are invalid at this point,
+    // try using them and see what compiler error you get!
+}
